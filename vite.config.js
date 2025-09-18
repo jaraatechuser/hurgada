25a69d4a1c0063fcc7f0321c50587e8aa214063d
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-09-18 17:02:23 +0400

    Home Page Implementation

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+        cors: {
+            origin: ['http://www.hurgada.local'],
+            credentials: false,
+        },
+        headers: {
+            'Access-Control-Allow-Origin': 'http://www.hurgada.local',
+        },
+        hmr: {
+            host: '127.0.0.1',
+            protocol: 'ws',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
